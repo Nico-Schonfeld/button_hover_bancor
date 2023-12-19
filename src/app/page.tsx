3fa886e5f4f293c20fd3608d02a08ff15d55f570
Nico-Schonfeld <nicoschonfeld88@gmail.com>
2023-12-19 20:25:56 -0300

    Efecto hover para el boton de bancor somos champaqui

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <main className="w-full h-screen flex items-center justify-center flex-col gap-10">
+      <h1 className="text-4xl font-bold">Test effect hover in button</h1>
+
+      <button className="btnn">
+        <span className="btnn-span">This is a button</span>
+      </button>
+    </main>
+  );
+};
+
+export default App;
